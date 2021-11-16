aa2bb947b2369d68cc29290ab4581b2ca0166c88
NotSimcraftLOL <92390528+RealNotSimcraftlol@users.noreply.github.com>
2021-11-16 18:42:26 +0100

    Updated to latest fn update

--- Offsets.h
+++ Offsets.h
@@ -29,5 +29,5 @@ namespace StaticOffsets {
 	uintptr_t RelativeRotation = 0x128;
 	uintptr_t PlayerState = 0x240;
 	uintptr_t Mesh = 0x280;
-	uintptr_t TeamIndex = 0xEE0;
+	uintptr_t TeamIndex = 0xF28;
 }
